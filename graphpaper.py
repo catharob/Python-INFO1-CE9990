f00b5544ec8cd89f9bf98a6921a08fb8db3269d9
Catherine Roberts <catharob@gmail.com>
2017-06-27 12:14:01 -0400

    added first graphpaper program

---
+++ graphpaper.py
@@ -0,0 +1,51 @@
+"""
+graphpaper.py
+
+Tell me how big you want the squares to be in your graph paper, and how much you'd like of it, and I'll print it out for you.
+
+"""
+
+import sys
+
+rows = int(input("How many rows of boxes? "))
+columns = int(input("How many columns of boxes? "))
+rSpace = int(input("How many rows of spaces in each box (e.g., 1)? "))
+cSpace = int(input("How many columns of spaces in each box (e.g., 3)? "))
+
+r = 1
+c = 1
+rs = 1
+cs = 1
+
+while r <= rows:
+	print("\n", end = "")
+
+	while rs <= rSpace:
+
+		while c <= columns:
+
+			print("+", end = "")
+
+			while cs <= cSpace:
+				print("-", end = "")
+				cs += 1
+
+			c += 1
+			cs = 1
+		c = 1
+		print("\n", end = "")
+
+		while c <= columns:
+			print("|", end = "")
+
+			while cs <= cSpace:
+				print(" ", end = "")
+				cs += 1 
+			c +=1
+			cs = 1
+		rs +=1
+	rs = 1
+	c = 1
+	r += 1
+
+sys.exit(0)
