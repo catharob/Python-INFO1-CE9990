3d82e7166077c9b71a69361dee679a81d676cfbe
Catherine Roberts <catharob@gmail.com>
2017-07-06 18:32:08 -0400

    replaced recusion with while loop

--- vowelMuncher.py
+++ vowelMuncher.py
@@ -7,26 +7,30 @@ It's a little vowel-eating monster.
 
 """
 
+import sys
+
 vowels = "aeiou"
 
-word = input("Please type a word: ")
 
+n = 10
 v = 0
 
 def eatTheVowels(word, v):
-	for w in word:
-		if w in vowels:
-			word = word.replace(w, "")
-			v +=1
+	while True:
+		for w in word:
+			if w in vowels:
+				word = word.replace(w, "")
+				v +=1
 
-	print("Yum, vowels! Here's what's left: "+ word)
-	if v < 10:
-		newword = input("Still hungry! Type another word: ")
-		eatTheVowels(newword, v)
-	else:
-		print("So full! Thanks for feeding me 10 vowels!")
+		print("Yum, vowels! Here's what's left: "+ word)
+		if v >= n:
+			print("So full! Thanks for feeding me", n, "vowels!")
+			break
+		word = input("Still hungry! Type another word: ")
 
+word = input("Please type a word: ")
 eatTheVowels(word, v)
+sys.exit(0)
 
 
 
