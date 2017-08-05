802360d12e32b5b3ce0595d62bb41c923ba0db85
Catherine Roberts <catharob@gmail.com>
2017-08-05 15:18:22 -0400

    added Mark's edits

--- csvNYCdisk.py
+++ csvNYCdisk.py
@@ -20,17 +20,19 @@ except PermissionError:
     print("Sorry, no permission to open file \"", filename, "\".", sep = "")
     sys.exit(1)
 
-zip = input("Please enter a New York City zip code: ")
+zipCode = input("Please enter a New York City zip code: ")
 print("One moment please...")
 noiseList = []
 
 lines = csv.reader(csvfile)
 
 for line in lines:              
-    if line[8] == zip and "Noise" in line[5]:  #finds noise complaints in zip code user chooses
+    if line[8] == zipCode and "Noise" in line[5]:  #finds noise complaints in zip code user chooses
         noiseList.append(line[5])  # adds zip codes to list to be counted
 
-print("There have been", len(noiseList), "noise complaints in", zip, "from the start of 2015 to July 19, 2017.")
-
 csvfile.close()
+
+print("There have been", len(noiseList), "noise complaints in", zipCode, "from the start of 2015 to July 19, 2017.")
+
+
 sys.exit(0)
